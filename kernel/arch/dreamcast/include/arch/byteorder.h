030ef479b529aad72f09603947175d11be5ff7d9
QuzarDC <quzar@completext.com>
2025-09-05 18:21:02 -0400

    byteorder: Remove doxygen for deprecated file.
    Additionally clear out the undef/redef of `BYTE_ORDER` in
    favor of the standard location for that macro.

--- kernel/arch/dreamcast/include/arch/byteorder.h
+++ kernel/arch/dreamcast/include/arch/byteorder.h
@@ -5,137 +5,44 @@
 
 */
 
-/** \file    arch/byteorder.h
-    \brief   Byte-order related macros.
-    \ingroup system_arch
-
-    This file contains architecture-specific byte-order related macros and/or
-    functions. Each platform should define six macros/functions in this file:
-    arch_swap16, arch_swap32, arch_ntohs, arch_ntohl, arch_htons, and
-    arch_htonl. The first two of these swap the byte order of 16-bit and 32-bit
-    integers, respectively. The other four macros will be used by the kernel to
-    implement the network-related byte order functions.
-
-    \author Lawrence Sebald
-*/
-
 #ifndef __ARCH_BYTEORDER_H
 #define __ARCH_BYTEORDER_H
 
 #include <kos/cdefs.h>
 __BEGIN_DECLS
 
-/* Included to get the `LITTLE_ENDIAN` define */
+/* Bring in the newlib header that defines the BYTE_ORDER macro */
 #include <machine/endian.h>
 
-#ifdef BYTE_ORDER
-/* If we've included <arch/types.h>, this might already be defined... */
-#undef BYTE_ORDER
-#endif
-
-/** \defgroup system_arch  Byte Order
-    \brief                 Byte-order management for the SH4 architecture
-    \ingroup               arch
-
-    @{
-*/
-
-/** \brief  Define the byte-order of the platform in use. */
-#define BYTE_ORDER      LITTLE_ENDIAN
-
-/** \brief  Swap the byte order of a 16-bit integer.
-
-    This macro swaps the byte order of a 16-bit integer in an architecture-
-    defined manner.
-
-    \param  x           The value to be byte-swapped. This should be a uint16,
-                        or equivalent.
-    \return             The swapped value.
-*/
 __depr("arch_swap16() is deprecated, use __builtin_bswap16().")
 static inline uint16_t arch_swap16(uint16_t x) {
     return __builtin_bswap16(x);
 }
 
-/** \brief  Swap the byte order of a 32-bit integer.
-
-    This macro swaps the byte order of a 32-bit integer in an architecture-
-    defined manner.
-
-    \param  x           The value to be byte-swapped. This should be a uint32,
-                        or equivalent.
-    \return             The swapped value.
-*/
 __depr("arch_swap32() is deprecated, use __builtin_bswap32().")
 static inline uint32_t arch_swap32(uint32_t x) {
     return __builtin_bswap32(x);
 }
 
-/** \brief  Convert network-to-host short.
-
-    This macro converts a network byte order (big endian) value to the host's
-    native byte order. On a little endian system (like the Dreamcast), this
-    should just call arch_swap16(). On a big endian system, this should be a
-    no-op.
-
-    \param  x           The value to be converted. This should be a uint16,
-                        or equivalent.
-    \return             The converted value.
-*/
 __depr("arch_ntohs() is deprecated, use ntohs() from <arpa/inet.h>")
 static inline uint16_t arch_ntohs(uint16_t x) {
     return __builtin_bswap16(x);
 }
 
-/** \brief  Convert network-to-host long.
-
-    This macro converts a network byte order (big endian) value to the host's
-    native byte order. On a little endian system (like the Dreamcast), this
-    should just call arch_swap32(). On a big endian system, this should be a
-    no-op.
-
-    \param  x           The value to be converted. This should be a uint32,
-                        or equivalent.
-    \return             The converted value.
-*/
 __depr("arch_ntohl() is deprecated, use ntohl() from <arpa/inet.h>")
 static inline uint32_t arch_ntohl(uint32_t x) {
     return __builtin_bswap32(x);
 }
 
-/** \brief  Convert host-to-network short.
-
-    This macro converts a value in the host's native byte order to network byte
-    order (big endian). On a little endian system (like the Dreamcast), this
-    should just call arch_swap16(). On a big endian system, this should be a
-    no-op.
-
-    \param  x           The value to be converted. This should be a uint16,
-                        or equivalent.
-    \return             The converted value.
-*/
 __depr("arch_htons() is deprecated, use htons() from <arpa/inet.h>")
 static inline uint16_t arch_htons(uint16_t x) {
     return __builtin_bswap16(x);
 }
 
-/** \brief  Convert host-to-network long.
-
-    This macro converts a value in the host's native byte order to network byte
-    order (big endian). On a little endian system (like the Dreamcast), this
-    should just call arch_swap32(). On a big endian system, this should be a
-    no-op.
-
-    \param  x           The value to be converted. This should be a uint32,
-                        or equivalent.
-    \return             The converted value.
-*/
 __depr("arch_htonl() is deprecated, use htonl() from <arpa/inet.h>")
 static inline uint32_t arch_htonl(uint32_t x) {
     return __builtin_bswap32(x);
 }
-
-/** @} */
 
 __END_DECLS
 
