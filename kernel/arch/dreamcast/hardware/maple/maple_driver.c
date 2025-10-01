5b26c5fef3ab3b716db33c70a8377e13076a9ec9
QuzarDC <quzar@completext.com>
2025-10-01 14:24:57 -0400

    maple: Stop directly accessing `LIST` struct member.
    This was previously being done in each driver, and I had
    refactored it to be tested at registration (#551), but
    it's just wrong. I had not dug into what the operation
    was doing when I refactored it, but it seems as though
    it was attempting to prevent a single driver from being
    added to more than one list (or into the same list twice).
    
    Removing as it's improper to directly access the members
    of these `LIST` macros, and there is now protection against
    not only adding the same driver twice but adding any two
    drivers that have functions overlapping.

--- kernel/arch/dreamcast/hardware/maple/maple_driver.c
+++ kernel/arch/dreamcast/hardware/maple/maple_driver.c
@@ -47,9 +47,6 @@ int maple_driver_reg(maple_driver_t *driver) {
     /* Don't add two drivers for the same function */
     maple_driver_t *i;
 
-    if(driver->drv_list.le_prev)
-        return -1;
-
     LIST_FOREACH(i, &maple_state.driver_list, drv_list)
         if(i->functions & driver->functions)
             return -1;
