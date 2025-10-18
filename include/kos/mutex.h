73296cbddb9750e2bf2eb8f3886dd407734d190f
Paul Cercueil <paul@crapouillou.net>
2025-10-18 11:51:58 +0200

    mutex: Deprecate error-checking mutexes
    All mutexes are error-checking now, provided that assertions are
    enabled.
    
    Signed-off-by: Paul Cercueil <paul@crapouillou.net>

--- include/kos/mutex.h
+++ include/kos/mutex.h
@@ -81,9 +81,11 @@ typedef struct kos_mutex {
 */
 #define MUTEX_TYPE_NORMAL       0   /**< \brief Normal mutex type */
 #define MUTEX_TYPE_OLDNORMAL    1   /**< \brief Alias for MUTEX_TYPE_NORMAL */
-#define MUTEX_TYPE_ERRORCHECK   2   /**< \brief Error-checking mutex type */
 #define MUTEX_TYPE_RECURSIVE    3   /**< \brief Recursive mutex type */
 #define MUTEX_TYPE_DESTROYED    4   /**< \brief Mutex that has been destroyed */
+
+ __depr("Error-checking mutexes are deprecated")
+static const unsigned int MUTEX_TYPE_ERRORCHECK = 2;
 
 /** \brief Default mutex type */
 #define MUTEX_TYPE_DEFAULT      MUTEX_TYPE_NORMAL
