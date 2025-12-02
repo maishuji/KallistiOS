7a933f35c0988213333f9cfce550f147f26ae8af
QuzarDC <quzar@completext.com>
2025-12-02 09:57:32 -0500

    mm: Use `uintptr_t` for pointer math.
    Storing `sbrk_base` as a `uintptr_t` means that only one
    cast is needed for returning a final `void *` rather than
    repeatedly for the alignment, incrementing, and testing of
    the value.
    
    This also allowed for the dropping of `arch/types.h` as it
    was only being used for some of that casting.

--- kernel/arch/dreamcast/kernel/mm.c
+++ kernel/arch/dreamcast/kernel/mm.c
@@ -14,46 +14,47 @@
 /* Note: right now we only support system RAM */
 
 #include <arch/arch.h>
-#include <arch/types.h>
 #include <arch/irq.h>
 #include <arch/stack.h>
 #include <kos/dbglog.h>
 #include <kos/linker.h>
 #include <errno.h>
+#include <inttypes.h>
+#include <stdint.h>
 #include <stdio.h>
 
 /* The end of the program is always marked by the '_end' symbol. So we'll
    longword-align that and add a little for safety. sbrk() calls will
    move up from there. */
-static void *sbrk_base;
+static uintptr_t sbrk_base;
 
 /* MM-wide initialization */
 int mm_init(void) {
-    int base = (int)end;
+    uintptr_t base = (uintptr_t)end;
     base = (base / 4) * 4 + 4;  /* longword align */
-    sbrk_base = (void*)base;
+    sbrk_base = base;
 
     return 0;
 }
 
 /* Simple sbrk function */
-void* mm_sbrk(unsigned long increment) {
-    void *base = sbrk_base;
+void *mm_sbrk(unsigned long increment) {
+    uintptr_t base = sbrk_base;
 
     irq_disable_scoped();
 
     if(increment & 3)
         increment = (increment + 4) & ~3;
 
-    sbrk_base = (void *)(increment + (unsigned long)sbrk_base);
+    sbrk_base += increment;
 
-    if(((uint32)sbrk_base) >= (_arch_mem_top - THD_KERNEL_STACK_SIZE)) {
-        dbglog(DBG_CRITICAL, "Out of memory. Requested sbrk_base %p, was %p, diff %lu\n",
-               sbrk_base, base, increment);
+    if(sbrk_base >= (_arch_mem_top - THD_KERNEL_STACK_SIZE)) {
+        dbglog(DBG_CRITICAL, "Out of memory. Requested sbrk_base %" PRIxPTR \
+            ", was %" PRIxPTR ", diff %lu\n", sbrk_base, base, increment);
         sbrk_base = base;  /* Restore old value and mark failed */
         errno = ENOMEM;
-        return (void*) -1;
+        return (void *)-1;
     }
 
-    return base;
+    return (void *)base;
 }
