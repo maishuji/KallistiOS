05aa1fa2eb0cff2f31b250051ec9cc30c2c64f56
QuzarDC <quzar@completext.com>
2026-01-02 04:23:06 -0500

    arch/irq: Properly type irq trap codes
    C++ is less friendly to this kind of implicit typing,
    so ensure to cast explicitly.

--- kernel/arch/dreamcast/include/arch/irq.h
+++ kernel/arch/dreamcast/include/arch/irq.h
@@ -230,7 +230,7 @@ enum irq_exception
     EXC_TRAP               = 0x0800  /**< `[TRAP  ]` Trap */
 };
 
-#define IRQ_TRAP_CODE(code) (EXC_TRAP + (code))
+#define IRQ_TRAP_CODE(code) (irq_t)(EXC_TRAP + (code))
 
 extern int inside_int;
 static inline int arch_irq_inside_int(void) {
