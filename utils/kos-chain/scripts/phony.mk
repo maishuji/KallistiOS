c25f1342d6ab9ff3215266b3c5ba7108591983c5
darc <darcagn@protonmail.com>
2026-02-07 01:31:26 -0600

    kos-chain: Clean up phony targets

--- utils/kos-chain/scripts/phony.mk
+++ utils/kos-chain/scripts/phony.mk
@@ -1,11 +1,10 @@
 # KallistiOS Toolchain Builder (kos-chain)
 
 .PHONY: all
-.PHONY: fetch fetch-gdb
+.PHONY: fetch
 .PHONY: patch $(patch_targets)
-.PHONY: $(patch_binutils) $(patch_gcc) $(patch_newlib)
 .PHONY: build $(build_targets)
 .PHONY: build-binutils build-newlib build-gcc-pass1 build-gcc-pass2 fixup-newlib
-.PHONY: gdb install-gdb build-gdb
+.PHONY: gdb install-gdb build-gdb patch-gdb fetch-gdb
 .PHONY: clean clean-builds clean-downloads
 .PHONY: distclean clean-builds clean-downloads clean-archives
